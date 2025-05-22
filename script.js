9c7847078b88f8746dc422488936395b2a19bd1f
Jatin <jatinverma2428@gmail.com>
2025-05-22 12:00:04 +0530

    img click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let images =["img/two.jpg","img/three.jpg","img/four.jpg","img/five.jpg","img/six.jpg","img/seven.jpg",];
+let img = document.getElementById("photo");
  let button = document.getElementById("btn")
  button.addEventListener("click",()=>{
  let index = Math.floor(Math.random()*images.length);
- photo.src = images[index];
+ img.src = images[index];
  });
